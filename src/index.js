ec6ca3599270099129d4b316c243005216cbd777
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-15 14:24:24 +0530

    imported blog,services,signup in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
+import { Blog } from "./pages/Blog";
+import { Services } from "./pages/Services";
+import { SignUp } from "./pages/SignUp";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
