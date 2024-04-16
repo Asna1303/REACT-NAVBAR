93a04103bfaf9f639668ac19538de3b7ac450717
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-16 21:39:02 +0530

    imported outlet from react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Navbar } from "./components/Navbar";
-
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Outlet />
     </div>
   );
 }
