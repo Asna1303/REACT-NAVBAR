b32225a5952f6888857bd949262611383a0f6801
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-18 20:46:32 +0530

    imported styled from styled components

--- src/styles/Navbar.styled.jsx
+++ src/styles/Navbar.styled.jsx
@@ -1,6 +1,1 @@
-import React from "react";
-import { LogoImg } from "../styles/Navbar.styled";
-import logo from "../assets/logo.svg";
-export const Logo = () => {
-  return <LogoImg src={logo} alt="logo" />;
-};
+import styled from "styled-components";
