8916db061ffa07d464a6b3e7767475ff68c828d5
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-19 11:31:40 +0530

    imported logoimg in logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { LogoImg } from '../styles/Navbar.styled'
 
 export const Logo = () => {
   return (
-    <div>
-      
-    </div>
+    <LogoImg>
+    </LogoImg>
   )
 }
 
