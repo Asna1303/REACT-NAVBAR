91bad0821433da17874a82731ca594df9a63485d
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-20 20:33:20 +0530

    styled navbarwrapper

--- src/styles/Navbar.styled.jsx
+++ src/styles/Navbar.styled.jsx
@@ -3,5 +3,17 @@ const LogoImg = styled.img`
   align-self: start;
   width: 150px;
 `;
-export {
-    LogoImg};
+const NavbarWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #000;
+  padding: 1rem 3rem;
+  position: relative;
+  @media (max-width: 704px) {
+    flex-direction: column;
+    padding: 1rem;
+  }
+`;
+
+export { LogoImg};
