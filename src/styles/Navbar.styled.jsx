b2a31c108391418a3813c3f7baadcee0404d04bd
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-21 18:14:03 +0530

    styled navlink

--- src/styles/Navbar.styled.jsx
+++ src/styles/Navbar.styled.jsx
@@ -22,5 +22,35 @@ const NavLinkWrapper = styled.div`
     padding: 2rem 0;
   }
 `;
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  transition: 0.2s;
+  color: white;
+  margin-left: 2rem;
+  &:last-child {
+    background: #007dfc;
+    padding: 0.5rem 1rem;
+    border-radius: 30px;
+    &:hover {
+      color: white;
+    }
+    &.${(props) => props.activeclassname} {
+      color: white;
+    }
+  }
+
+  &.${(props) => props.activeclassname} {
+    color: #007dfc;
+  }
+
+  &:hover {
+    color: #007dfc;
+  }
+
+  @media (max-width: 704px) {
+    display: block;
+    margin: 2rem auto;
+  }
+`;
 
 export { LogoImg};
