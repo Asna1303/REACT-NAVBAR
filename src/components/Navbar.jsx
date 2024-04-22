a9021b5faa5c81a8c5ea006df84bd30a889e2e7d
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-22 22:56:17 +0530

    added the link function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Logo } from './Logo'
-import { NavbarWrapper } from '../styles/Navbar.styled'
+import { NavLinkWrapper, NavbarWrapper } from '../styles/Navbar.styled'
 
 export const Navbar = () => {
+  const link = [
+    {
+      page: "Home",
+      href: "/",
+    },
+    { page: "About", href: "/about" },
+    { page: "Blog", href: "/blog" },
+    {
+      page: "Services",
+      href: "/services",
+    },
+    { page: "Sign Up", href: "/sign-up" },
+  ];
   return (
     <NavbarWrapper>
       <Logo/>
+      <NavLinkWrapper>
+
+      </NavLinkWrapper>
     </NavbarWrapper>
   )
 }
