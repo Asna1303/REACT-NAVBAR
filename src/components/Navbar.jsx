7f8556427576d754619ea9d2421908beb4349886
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-22 22:53:09 +0530

    imported navbarwrapper in navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Logo } from './Logo'
+import { NavbarWrapper } from '../styles/Navbar.styled'
 
 export const Navbar = () => {
   return (
-    <div>
+    <NavbarWrapper>
       <Logo/>
-    </div>
+    </NavbarWrapper>
   )
 }
 
